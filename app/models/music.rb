cfad6392ecbff63f7ed9fb3d087ec584961ec220
justine <justine.winnie@gmail.com>
2016-05-20 13:36:04 -0700

    a music thing displays.

--- app/models/music.rb
+++ app/models/music.rb
@@ -1,43 +1,41 @@
+# Includes name of the music item, linking to appropriate Spotify page
+# - Includes type of the music item (artist, album, track, or playlist)
+# - Includes photo/album cover of the music item
+# - Includes embedded Spotify player if appropriate (optional)
+
 require 'rspotify'
 
 class Music < ActiveRecord::Base
   # receives and models data retrieved from the Spotify API
   BASE_URL = "https://api.spotify.com"
-  attr_reader :artist, :album, :track, :music
+  attr_reader :artist, :album, :track, :music, :name
 
-  def initialize(data = {})
+  def initialize(music_object)
       # @album = data[:album],
       # @artist = data[:artist],
       # @track = data[:track]
 
-      @music =data[:music]
+      # @music = data[:music]
+      @name = music_object.name
   end
 
   def self.search(music_id, music_type)
-    # music_type == "album" ? @album = RSpotify::Album.find(music_id) : @album = nil
-    # music_type == "artist" ? @artist = RSpotify::Artist.find(music_id) : @artist = nil
-    # music_type == "track" ? @track = RSpotify::Track.find(music_id) : @track = nil
+    if music_type == "album"
+      music_object = RSpotify::Album.find(music_id)
+    elsif music_type == "artist"
+      music_object = RSpotify::Artist.find(music_id)
+    elsif music_type == "track"
+      music_object = RSpotify::Track.find(music_id)
+    else
+      music_object = nil
+    end
 
-    music_type == "album" ? @music = RSpotify::Album.find(music_id) : @music = nil
-    music_type == "artist" ? @music = RSpotify::Artist.find(music_id) : @music = nil
-    music_type == "track" ? @music = RSpotify::Track.find(music_id) : @music = nil
+    # data = {}
+    # # data[:album] = @album
+    # # data[:artist] = @artist
+    # # data[:track] = @track
+    # data[:music] = @music
 
-    data = {}
-    # data[:album] = @album
-    # data[:artist] = @artist
-    # data[:track] = @track
-    data[:music] = @music
-
-    self.new(data)
+    self.new(music_object)
   end
-
-  # def music.display(music_object)
-  #
-  # end
-
 end
-
-# Includes name of the music item, linking to appropriate Spotify page
-# - Includes type of the music item (artist, album, track, or playlist)
-# - Includes photo/album cover of the music item
-# - Includes embedded Spotify player if appropriate (optional)
