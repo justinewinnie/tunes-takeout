19c3839ee422092970c552e345200093cdc68f80
justine <justine.winnie@gmail.com>
2016-05-22 11:21:06 -0700

    replacing find_or_create user method after accidentally discarding

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,4 @@
-require 'pry'
+# require 'pry'
 class SessionsController < ApplicationController
 
   def new
@@ -7,9 +7,8 @@ class SessionsController < ApplicationController
 
   def create
     auth_hash = request.env['omniauth.auth']
-    # binding.pry
     if auth_hash[:uid]
-      @user = User.find_or_create_by_omniauth(auth_hash)
+      @user = User.find_or_create_from_omniauth(auth_hash)
       if @user
         session[:user_id] = @user.id
         redirect_to root_path
@@ -25,8 +24,5 @@ class SessionsController < ApplicationController
     session.delete :user_id
     redirect_to root_path
   end
-  #
-  # def failure
-  #   render :text => "Sorry, but you didn't allow access to our app!"
-  # end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,26 @@ class User < ActiveRecord::Base
 
   def self.find_or_create_from_omniauth(auth_hash)
     # Find or create a user
+    user = self.find_by(provider: auth_hash[:provider], uid: auth_hash[:uid])
+
+    if user != nil
+      return user
+    else
+      user = User.new
+      user.uid = auth_hash["uid"]
+      user.provider = auth_hash["provider"]
+      user.name = auth_hash["info"]["name"]
+      user.email = auth_hash["info"]["email"]
+      user.image = auth_hash["info"]["image"]
+      if user.save
+        return user
+      else
+        return nil
+      end
+    end
   end
+
+
 
 #   Validations:
 # provider must be a string, must be present, and must equal spotify
